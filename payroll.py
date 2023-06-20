e9f47e7f7fea6b27099818caf7c84333781ce705
unknown <mohamadpanahinik@gmail.com>
2023-06-21 01:16:50 +0330

    create a new menu for payroll app

--- payroll.py
+++ payroll.py
@@ -7,39 +7,40 @@ class MainPage(Frame):
         self.main_menu()
 
     def main_menu(self):
-        self.addListBtnImg = PhotoImage(file='images/list.png')
-        self.addPersonnelImg = PhotoImage(file='images/register.png')
-        self.paySlipImg = PhotoImage(file='images/payroll.png')
-        btn_color='#BBBBBB'
-        self.y=250
+        self.configure(bg='#191818')
+        self.addListBtnImg = PhotoImage(file='images/list_img.png')
+        self.addPersonnelImg = PhotoImage(file='images/add_personell_img.png')
+        self.payroll_img = PhotoImage(file='images/payroll_img.png')
         
         header_menu_lbl = LabelFrame(width=1200,height=80,bg='#BEC8CF',bd=5,relief='solid')
         header_menu_txt = Label(text='PAYROLL',font=('B Titr',23,'bold'),bg='#BEC8CF')
-        self.frm_list = LabelFrame(width=400,height=620,bg=btn_color,bd=5,relief='solid',cursor='hand2')
-        img_list = Label(width=150,height=150,image=self.addListBtnImg,bg=btn_color)
-        txt_list = Label(text='لیست کارکنان',font=('B Titr',23,'bold'),bg=btn_color)
-        self.frm_add_personnel = LabelFrame(width=400,height=620,bg=btn_color,bd=5,relief='solid',cursor='hand2')
-        img_add_personnel = Label(width=150,height=150,image=self.addPersonnelImg,bg=btn_color)
-        txt_add_personnel = Label(text='ثبت کارمند جدید',font=('B Titr',23,'bold'),bg=btn_color)
-        self.frm_pay_slip = LabelFrame(width=400,height=620,bg=btn_color,bd=5,relief='solid',cursor='hand2')
-        img_pay_slip = Label(width=150,height=150,image=self.paySlipImg,bg=btn_color)
-        txt_pay_slip = Label(text='ثبت کارمند جدید',font=('B Titr',23,'bold'),bg=btn_color)
         
-        # self.frm_list.bind('<ENTER>',self.h_btn_menu)
+        self.btn_list = Button(width=400,height=620,image=self.addListBtnImg,bd=0,cursor='hand2',activebackground='#191818',command=self.open_receipt)
+        self.btn_add_personnel = Button(width=400,height=620,image=self.addPersonnelImg,bd=0,cursor='hand2',activebackground='#191818')
+        self.btn_payroll = Button(width=400,height=620,image=self.payroll_img,bd=0,cursor='hand2',activebackground='#191818')
         
-        header_menu_txt.place(x=530,y=15)
+        
+        header_menu_txt.place(x=510,y=15)
         header_menu_lbl.place(x=0,y=0)
-        self.frm_list.place(x=0,y=80)
-        img_list.place(x=125,y=250)
-        txt_list.place(x=110,y=400)
-        self.frm_add_personnel.place(x=400,y=80)
-        img_add_personnel.place(x=525,y=250)
-        txt_add_personnel.place(x=510,y=400)
-        self.frm_pay_slip.place(x=800,y=80)
-        img_pay_slip.place(x=925,y=250)
-        txt_pay_slip.place(x=910,y=400)
+        self.btn_list.place(x=0,y=80)
+        self.btn_add_personnel.place(x=400,y=80)
+        self.btn_payroll.place(x=800,y=80)
 
+    def open_receipt(self):
+        self.master.switch_frame(receipt)
+        
+class receipt(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        header_menu_lbl = LabelFrame(width=1200,height=80,bg='red',bd=5,relief='solid')
+        header_menu_txt = Label(text='PAYROLL',font=('B Titr',23,'bold'),bg='#BEC8CF')
+        header_menu_txt.place(x=510,y=15)
+        header_menu_lbl.place(x=0,y=0)
+        
     
+        
+        
 class App(Tk):
     def __init__(self):
         super().__init__()
